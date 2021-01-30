52592ffa3c149c9055156670134cb195e600b2e7
Jekyll <kalugin@outlook.com>
2021-01-30 23:41:43 +0300

    Add project files.

---
+++ IdleMove/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace IdleMove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox_enable = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            this.checkBox_enableHotKey = new System.Windows.Forms.CheckBox();
+            this.checkBox_enableSchedule = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_from = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_to = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // checkBox_enable
+            // 
+            this.checkBox_enable.AutoSize = true;
+            this.checkBox_enable.Location = new System.Drawing.Point(12, 12);
+            this.checkBox_enable.Name = "checkBox_enable";
+            this.checkBox_enable.Size = new System.Drawing.Size(59, 17);
+            this.checkBox_enable.TabIndex = 0;
+            this.checkBox_enable.Text = "Enable";
+            this.checkBox_enable.UseVisualStyleBackColor = true;
+            this.checkBox_enable.CheckedChanged += new System.EventHandler(this.CheckBox_enable_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Idle time";
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Location = new System.Drawing.Point(61, 35);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(69, 20);
+            this.textBox_time.TabIndex = 2;
+            this.textBox_time.Leave += new System.EventHandler(this.TextBox_time_Leave);
+            // 
+            // checkBox_enableHotKey
+            // 
+            this.checkBox_enableHotKey.AutoSize = true;
+            this.checkBox_enableHotKey.Location = new System.Drawing.Point(12, 61);
+            this.checkBox_enableHotKey.Name = "checkBox_enableHotKey";
+            this.checkBox_enableHotKey.Size = new System.Drawing.Size(118, 17);
+            this.checkBox_enableHotKey.TabIndex = 0;
+            this.checkBox_enableHotKey.Text = "Enable ESC hotkey";
+            this.checkBox_enableHotKey.UseVisualStyleBackColor = true;
+            this.checkBox_enableHotKey.CheckedChanged += new System.EventHandler(this.CheckBox_enableHotKey_CheckedChanged);
+            // 
+            // checkBox_enableSchedule
+            // 
+            this.checkBox_enableSchedule.AutoSize = true;
+            this.checkBox_enableSchedule.Location = new System.Drawing.Point(12, 84);
+            this.checkBox_enableSchedule.Name = "checkBox_enableSchedule";
+            this.checkBox_enableSchedule.Size = new System.Drawing.Size(105, 17);
+            this.checkBox_enableSchedule.TabIndex = 0;
+            this.checkBox_enableSchedule.Text = "Enable schedule";
+            this.checkBox_enableSchedule.UseVisualStyleBackColor = true;
+            this.checkBox_enableSchedule.CheckedChanged += new System.EventHandler(this.CheckBox_enableSchedule_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "From";
+            // 
+            // textBox_from
+            // 
+            this.textBox_from.Location = new System.Drawing.Point(45, 107);
+            this.textBox_from.Name = "textBox_from";
+            this.textBox_from.Size = new System.Drawing.Size(85, 20);
+            this.textBox_from.TabIndex = 2;
+            this.textBox_from.Leave += new System.EventHandler(this.TextBox_from_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "To";
+            // 
+            // textBox_to
+            // 
+            this.textBox_to.Location = new System.Drawing.Point(45, 133);
+            this.textBox_to.Name = "textBox_to";
+            this.textBox_to.Size = new System.Drawing.Size(85, 20);
+            this.textBox_to.TabIndex = 2;
+            this.textBox_to.Leave += new System.EventHandler(this.TextBox_to_Leave);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.NotifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(144, 172);
+            this.Controls.Add(this.textBox_to);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_from);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_time);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox_enableSchedule);
+            this.Controls.Add(this.checkBox_enableHotKey);
+            this.Controls.Add(this.checkBox_enable);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IdleMove";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox_enable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_time;
+        private System.Windows.Forms.CheckBox checkBox_enableHotKey;
+        private System.Windows.Forms.CheckBox checkBox_enableSchedule;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_from;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_to;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ IdleMove/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace IdleMove
+{
+    public partial class Form1 : Form
+    {
+        private const string ConfigFileName = "config.ini";
+        private const string EnableConfigItem = "Enable";
+        private const string EnableHotKeyConfigItem = "EnableHotKey";
+        private const string EnableScheduleConfigItem = "EnableSchedule";
+        private const string IdleDelayConfigItem = "IdleDelay";
+        private const string FromTimeConfigItem = "FromTime";
+        private const string ToTimeConfigItem = "ToTime";
+
+        private Hotkeys _ghk;
+        private int _minPoint = 10;
+        private int _maxPoint = 50;
+        private int _idleTime = 60;
+        private uint _lastIdleTime;
+        private bool _hotKeyEnabled;
+        private bool _scheduleEnabled;
+        private readonly Timer _systemTimer = new Timer();
+        private int _cursorMoveDelay;
+        private readonly Random _rnd = new Random();
+        private int _toX, _toY;
+        private DateTime _fromTime, _toTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+            var autoEnable = false;
+            try
+            {
+                var configLines = File.ReadAllLines(ConfigFileName);
+                foreach (var line in configLines)
+                {
+                    var l = line.Split('=');
+                    if (l.Length < 2) continue;
+
+                    l[0] = l[0].Trim().ToUpper();
+                    if (l[0] == EnableConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out var value);
+                        autoEnable = value > 0;
+                    }
+                    else if (l[0] == EnableHotKeyConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out var value);
+                        _hotKeyEnabled = value > 0;
+                    }
+                    else if (l[0] == EnableScheduleConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out var value);
+                        _scheduleEnabled = value > 0;
+                    }
+                    else if (l[0] == IdleDelayConfigItem.ToUpper())
+                    {
+                        int.TryParse(l[1], out _idleTime);
+                    }
+                    else if (l[0] == FromTimeConfigItem.ToUpper())
+                    {
+                        DateTime.TryParse(l[1], out _fromTime);
+                    }
+                    else if (l[0] == ToTimeConfigItem.ToUpper())
+                    {
+                        DateTime.TryParse(l[1], out _toTime);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Can't load settings: " + exception);
+            }
+
+            textBox_time.Text = _idleTime.ToString();
+            textBox_from.Text = _fromTime.ToShortTimeString();
+            textBox_to.Text = _toTime.ToShortTimeString();
+
+            _cursorMoveDelay = _rnd.Next(0, _idleTime);
+            _toX = _toY = _minPoint;
+
+            _systemTimer.Enabled = false;
+            _systemTimer.Interval = 1000;
+            _systemTimer.Tick += TimerEvent;
+
+            checkBox_enableHotKey.Checked = _hotKeyEnabled;
+            checkBox_enableSchedule.Checked = _scheduleEnabled;
+
+            checkBox_enable.Checked = autoEnable;
+            if (autoEnable)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var configText = "";
+            configText += EnableConfigItem + "=" + (_systemTimer.Enabled == true ? 1 : 0) + Environment.NewLine;
+            configText += EnableHotKeyConfigItem + "=" + (_hotKeyEnabled == true ? 1 : 0) + Environment.NewLine;
+            configText += EnableScheduleConfigItem + "=" + (_scheduleEnabled == true ? 1 : 0) + Environment.NewLine;
+            configText += IdleDelayConfigItem + "=" + _idleTime + Environment.NewLine;
+            configText += FromTimeConfigItem + "=" + _fromTime.ToShortTimeString() + Environment.NewLine;
+            configText += ToTimeConfigItem + "=" + _toTime.ToShortTimeString() + Environment.NewLine;
+            try
+            {
+                File.WriteAllText(ConfigFileName, configText);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Can't save settings: " + exception);
+            }
+        }
+
+        private void NotifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void CheckBox_enable_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_enable.Checked)
+            {
+                // start TimerEvent
+                _systemTimer.Enabled = true;
+
+
+                // register hotkey
+                if (_hotKeyEnabled)
+                {
+                    _ghk = new Hotkeys(Hotkeys.Constants.NOMOD, Keys.Escape, this);
+                    _ghk.Register();
+                }
+
+                // open form window
+                textBox_time.Enabled = false;
+            }
+            else
+            {
+                // stop timer
+                _systemTimer.Enabled = false;
+
+                // unregister hotkey
+                if (_hotKeyEnabled) _ghk.Unregister();
+
+                // minimize form
+                textBox_time.Enabled = true;
+                notifyIcon1.Text = "Inactive";
+            }
+        }
+
+        private void CheckBox_enableHotKey_CheckedChanged(object sender, EventArgs e)
+        {
+            _hotKeyEnabled = checkBox_enableHotKey.Checked;
+
+            if (_systemTimer.Enabled)
+            {
+                if (_hotKeyEnabled)
+                {
+                    _ghk = new Hotkeys(Hotkeys.Constants.NOMOD, Keys.Escape, this);
+                    _ghk.Register();
+                }
+                else
+                {
+                    if (_hotKeyEnabled) _ghk.Unregister();
+                }
+            }
+        }
+
+        private void CheckBox_enableSchedule_CheckedChanged(object sender, EventArgs e)
+        {
+            _scheduleEnabled = checkBox_enableSchedule.Checked;
+            textBox_from.Enabled = !_scheduleEnabled;
+            textBox_to.Enabled = !_scheduleEnabled;
+        }
+
+        private void TextBox_time_Leave(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox_time.Text, out _idleTime)) textBox_time.Text = _idleTime.ToString();
+
+            _cursorMoveDelay = _idleTime;
+        }
+
+        private void TextBox_from_Leave(object sender, EventArgs e)
+        {
+            if (!DateTime.TryParse(textBox_from.Text, out _fromTime)) textBox_from.Text = _fromTime.ToShortTimeString();
+
+            if (_fromTime.TimeOfDay >= _toTime.TimeOfDay)
+            {
+                _toTime = _fromTime.AddMinutes(1);
+                textBox_to.Text = _toTime.ToShortTimeString();
+            }
+        }
+
+        private void TextBox_to_Leave(object sender, EventArgs e)
+        {
+            if (!DateTime.TryParse(textBox_to.Text, out _toTime)) textBox_to.Text = _toTime.ToShortTimeString();
+
+            if (_toTime.TimeOfDay <= _fromTime.TimeOfDay)
+            {
+                _fromTime = _toTime.Subtract(new TimeSpan(0, 1, 0));
+                textBox_from.Text = _fromTime.ToShortTimeString();
+            }
+        }
+
+        private void TimerEvent(object sender, EventArgs args)
+        {
+            if (_scheduleEnabled)
+            {
+                var now = DateTime.Now;
+
+                if (now.TimeOfDay < _fromTime.TimeOfDay || now.TimeOfDay > _toTime.TimeOfDay) return;
+            }
+
+            var currentIdleTime = GetIdleTime();
+
+            if (currentIdleTime < _lastIdleTime)
+            {
+                _cursorMoveDelay = _idleTime;
+                _lastIdleTime = currentIdleTime;
+            }
+
+            if (currentIdleTime - _lastIdleTime > _cursorMoveDelay * 1000)
+            {
+                // move mouse
+                MoveCursor(_toX, _toY);
+                if (_toX != _minPoint) _toX = _toY = _minPoint;
+                else _toX = _toY = _maxPoint;
+
+                // refresh timer delay random
+                _cursorMoveDelay = _rnd.Next(0, _idleTime - 1);
+                _lastIdleTime = currentIdleTime;
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+            {
+                _systemTimer.Enabled = false;
+
+                if (_hotKeyEnabled) _ghk.Unregister();
+
+                Invoke((MethodInvoker)delegate
+                {
+                    checkBox_enable.Checked = false;
+                    notifyIcon1.Text = "Inactive";
+                    Show();
+                    notifyIcon1.Visible = false;
+                    WindowState = FormWindowState.Normal;
+                });
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private static uint GetIdleTime()
+        {
+            var lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return (uint)Environment.TickCount - lastInPut.dwTime;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO
+        {
+            public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+
+            [MarshalAs(UnmanagedType.U4)] public uint cbSize;
+            [MarshalAs(UnmanagedType.U4)] public uint dwTime;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        private static int GetLastInputTime()
+        {
+            var idleTime = 0;
+            var lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            var envTicks = Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                var lastInputTick = (int)lastInputInfo.dwTime;
+
+                idleTime = envTicks - lastInputTick;
+            }
+
+            return idleTime > 0 ? idleTime / 1000 : 0;
+        }
+
+        private void MoveCursor(int x, int y)
+        {
+            Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(x, y);
+        }
+    }
+}
